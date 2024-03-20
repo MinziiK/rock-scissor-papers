50419949434a286be6e073e6e1cc0077602ddfc4
MinziiK <zxcv923111@gmail.com>
2024-03-20 23:20:39 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -22,21 +22,70 @@ const choice = {
   paper: {
     name: "Paper",
     img: "https://cdn-icons-png.flaticon.com/256/2541/2541988.png"
+  },
+  default: {
+    name: "Default",
+    img: "https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/symbol_questionmark.png"
   }
 }
 
 function App() {
-  const [userSelect, setUserSelect] = useState(null);
+  const [userSelect, setUserSelect] = useState(choice.default);
+  const [computerSelect, setComputerSelect] = useState(choice.default);
+  const [result, setResult] = useState("initial");
+  const [comResult, setComResult] = useState("initial");
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    setResult(judgement(choice[userChoice], computerChoice));
+    setComResult(comJudgement(judgement(choice[userChoice], computerChoice)));
   };
+
+  const judgement = (user, computer) => {
+    if(user.name === computer.name){
+      return "tie"
+    }
+    else if(user.name === "Rock"){
+      return computer.name === "Scissors"? "win":"lose"
+    }
+    else if(user.name === "Scissors"){
+      return computer.name === "Paper"? "win":"lose"
+    }
+    else if(user.name === "Paper"){
+      return computer.name === "Rock"? "win":"lose"
+    }
+  }
+
+  const comJudgement = (result) => {
+    if (result === "tie") {
+      return "tie"
+    }
+    else if (result === "win"){
+      return "lose"
+    }
+    else if (result === "lose") {
+      return "win"
+    }
+  }
+
+  const randomChoice =()=>{
+    let itemArray = Object.keys(choice);  // 객체에 키 값만 뽑아서 어레이로 만들어주는 함수.
+    let randomItem = Math.floor(Math.random()*(itemArray.length-1));
+    let final = itemArray[randomItem]
+
+    return choice[final];
+  }
 
   return (
     <div>
+      <div className="title">
+        <h1>가위바위보 게임!</h1>
+      </div>
       <div className="main">
-        <Box title="YOU" item={userSelect}/>
-        {/* <Box title="COMPUTER"/> */}
+        <Box title="YOU" item={userSelect} result={result}/>
+        <Box title="COMPUTER" item={computerSelect} result={comResult}/>
       </div>
       <div className="main">
         <button onClick={() => play("scissors")}>가위</button>
--- src/component/Box.js
+++ src/component/Box.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const Box = (props) => {
-    // console.log("props: ", props)
+
   return (
-    <div className="box">
-        <h1>{props.title}</h1>
-        <img 
-            src={props.item && props.item.img}
-            className="item-img"/>
-        <h2>Win!</h2>
+    <div className={props.result}>
+      <h1>{props.title}</h1>
+      <img 
+          src={props.item && props.item.img}
+          className="item-img"/>
+      <h2>{props.result}</h2>
     </div>
   )
 }
